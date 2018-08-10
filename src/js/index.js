a8e7cb4b4bb28ed00e4a9d46dbfa2edd38a72e67
justinenglish <justnenglish@gmail.com>
2018-08-10 19:53:09 -0400

    Added opacity scroll functionality to header

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 import "../scss/main.scss";
 import "bootstrap";
 import "vendor/js/fontawesome-all.min.js";
+
+$(window).scroll(function() {
+    $(".header .header-content, .header .down-arrow").css("opacity", 1 - $(window).scrollTop() / 800);
+});
