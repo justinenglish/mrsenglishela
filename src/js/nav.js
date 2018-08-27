64e7f2e199b6146616d6ad8acbd1384407abe506
justinenglish <justnenglish@gmail.com>
2018-08-27 19:16:54 -0400

    Added mobile nav; updated header bg path

--- src/js/nav.js
+++ src/js/nav.js
@@ -7,6 +7,18 @@ function scrollTo(target, offset) {
     }, 1000);
 }
 
+$(".header-nav__mobile").on("click", function() {
+    // $(this).children("div").toggleClass("open");  
+    // $(".header-nav__mobile-list").toggleClass("display-none");
+    $(".header-nav__mobile-list").css("width", "250px");
+    $(".header-nav__mobile-list--close").css("opacity", "1");
+});
+
+$(".header-nav__mobile-list--close").on("click", function() {
+    $(".header-nav__mobile-list").css("width", "0px");
+    $(this).css("opacity", "0");
+});
+
 $(".header-nav li a, .down-arrow").click(function(e) {
     e.preventDefault();
     scrollTo($(this).attr("href"), 0);
